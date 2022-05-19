f2484055e803fa79407056ff77751049c238ab3d
Darlan Cavalcante Moreira <darcamo@gmail.com>
2022-05-19 11:15:50 -0300

    Set logview to work with pcoip logs

--- custom.el
+++ custom.el
@@ -3,8 +3,28 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(ignored-local-variable-values
+   '((pyvenv-workon . "git_files/cppsim/apps/iab/process_iab_results/.venv")))
+ '(logview-additional-level-mappings
+   '(("PCoIP-Levels"
+      (error "LVL:0")
+      (warning "LVL:1")
+      (information "LVL:2")
+      (debug "LVL:3")
+      (trace "LVL:4")
+      (aliases "pcoip"))))
+ '(logview-additional-submodes
+   '(("PCoIP"
+      (format . "TIMESTAMP LEVEL THREAD IGNORED NAME")
+      (levels . "PCoIP-Levels")
+      (timestamp)
+      (aliases))))
+ '(logview-additional-timestamp-formats
+   '(("PCoIP-Timestamp"
+      (regexp . "[0-9]+-[0-9][0-9]-[0-9][0-9]T[0-9][0-9]:[0-9][0-9]:[0-9][0-9]\\.[0-9][0-9][0-9]Z"))))
  '(package-selected-packages
    '(org-journal blamer all-the-icons-ibuffer all-the-icons-dired all-the-icons-completion calibredb nov org-remark ace-window biblio lsp-pyright mu4e-column-faces pyvenv mu4e sudo-edit svg-tag-mode lorem-ipsum conda yasnippet-snippets dockerfile-mode git-timemachine citar org-contrib anzu doom-themes flycheck doom-modeline unfill org-roam-ui org-download dashboard highlight-symbol ninja-mode smartparens expand-region diminish org-fragtog indent-guide origami kind-icon consult-yasnippet yasnippet yasnippets corfu auctex-latexmk auctex consult-lsp cmake-mode highlight-doxygen treemacs-icons-dired dired-icon dired-git lsp-mode lsp consult-dir which-key embark-consult embark org-superstar marginalia orderless vertico org-pdftools pdf-tools auto-package-update comment-dwim-2 git-gutter rainbow-delimiters yaml-mode magit adaptive-wrap rainbow-mode org-roam-bibtex org-roam drag-stuff org-super-agenda ace-mc multiple-cursors discover-my-major goto-last-change page-break-lines page-break-mode helm-command helm use-package))
+ '(safe-local-variable-values '((cmake-integration-run-working-directory . bin)))
  '(warning-suppress-log-types '((comp))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
