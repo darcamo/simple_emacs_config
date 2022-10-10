9ba97b41ae95cfd86277998d4e5ea9c81fbac762
Darlan Cavalcante Moreira <darcamo@gmail.com>
2022-10-10 18:54:06 -0300

    Update package-selected-packages variable and safe local variables

--- custom.el
+++ custom.el
@@ -23,8 +23,16 @@
    '(("PCoIP-Timestamp"
       (regexp . "[0-9]+-[0-9][0-9]-[0-9][0-9]T[0-9][0-9]:[0-9][0-9]:[0-9][0-9]\\.[0-9][0-9][0-9]Z"))))
  '(package-selected-packages
-   '(org-journal blamer all-the-icons-ibuffer all-the-icons-dired all-the-icons-completion calibredb nov org-remark ace-window biblio lsp-pyright mu4e-column-faces pyvenv mu4e sudo-edit svg-tag-mode lorem-ipsum conda yasnippet-snippets dockerfile-mode git-timemachine citar org-contrib anzu doom-themes flycheck doom-modeline unfill org-roam-ui org-download dashboard highlight-symbol ninja-mode smartparens expand-region diminish org-fragtog indent-guide origami kind-icon consult-yasnippet yasnippet yasnippets corfu auctex-latexmk auctex consult-lsp cmake-mode highlight-doxygen treemacs-icons-dired dired-icon dired-git lsp-mode lsp consult-dir which-key embark-consult embark org-superstar marginalia orderless vertico org-pdftools pdf-tools auto-package-update comment-dwim-2 git-gutter rainbow-delimiters yaml-mode magit adaptive-wrap rainbow-mode org-roam-bibtex org-roam drag-stuff org-super-agenda ace-mc multiple-cursors discover-my-major goto-last-change page-break-lines page-break-mode helm-command helm use-package))
- '(safe-local-variable-values '((cmake-integration-run-working-directory . bin)))
+   '(yapfify dap-mode jupyter lsp-latex ox-reveal qml-mode yaml org-journal blamer all-the-icons-ibuffer all-the-icons-dired all-the-icons-completion calibredb nov org-remark ace-window biblio lsp-pyright mu4e-column-faces pyvenv mu4e sudo-edit svg-tag-mode lorem-ipsum conda yasnippet-snippets dockerfile-mode git-timemachine citar org-contrib anzu doom-themes flycheck doom-modeline unfill org-roam-ui org-download dashboard highlight-symbol ninja-mode smartparens expand-region diminish org-fragtog indent-guide origami kind-icon consult-yasnippet yasnippet yasnippets corfu auctex-latexmk auctex consult-lsp cmake-mode highlight-doxygen treemacs-icons-dired dired-icon dired-git lsp-mode lsp consult-dir which-key embark-consult embark org-superstar marginalia orderless vertico org-pdftools pdf-tools auto-package-update comment-dwim-2 git-gutter rainbow-delimiters yaml-mode magit adaptive-wrap rainbow-mode org-roam-bibtex org-roam drag-stuff org-super-agenda ace-mc multiple-cursors discover-my-major goto-last-change page-break-lines page-break-mode helm-command helm use-package))
+ '(safe-local-variable-values
+   '((pyvenv-workon . "git_files/cppsim/apps/iab/process_iab_results/.venv")
+     (cmake-integration-hide-utility-targets-during-completion)
+     (cmake-integration-include-conan-toolchain-file)
+     (cmake-integration-include-conan-toolchain-file . t)
+     (cmake-integration-conan-profile . "gcc-release")
+     (darlan-cmake-use-ninja . t)
+     (darlan-build-dir . "build")
+     (cmake-integration-run-working-directory . bin)))
  '(warning-suppress-log-types '((comp))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
