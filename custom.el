b56b186e6f3f53d8c04634eb57ff82e30edbdc31
Darlan Cavalcante Moreira <darcamo@gmail.com>
2026-09-03 20:40:55 -0300

    Small changes

--- custom.el
+++ custom.el
@@ -7,27 +7,22 @@
  '(custom-safe-themes
    '("920a9ae46eff9e641e6af864a09201767f4aca9cf169b2287d9a701ef5ecaa87"
      "cc3cb3b291a7e31f1d40ea8587fb2907aca168d820ab4683764f7c478761f84f"
-     "f60670b53f340f48be6071dd39e70e50e634261e6178e95f28f329bea36919ec"
-     default))
+     "f60670b53f340f48be6071dd39e70e50e634261e6178e95f28f329bea36919ec" default))
+ '(ignored-local-variable-values '((system-time-locale . "C")))
  '(package-selected-packages nil)
  '(safe-local-variable-values
-   '((compilation-search-extra-path "./" "./agent"
-                                    "./agent/catalog_client"
-                                    "./agent/cmd"
-                                    "./agent/peer_client"
-                                    "./agent/server" "./agent/store"
-                                    "./catalog" "./catalog/cmd"
-                                    "./catalog/server"
+   '((compilation-search-extra-path "./" "./agent" "./agent/catalog_client"
+                                    "./agent/cmd" "./agent/peer_client"
+                                    "./agent/server" "./agent/store" "./catalog"
+                                    "./catalog/cmd" "./catalog/server"
                                     "./example_server/cmd"
                                     "./example_server/server"
-                                    "./internal/cache_config"
-                                    "./internal/fs"
+                                    "./internal/cache_config" "./internal/fs"
                                     "./internal/storage" "server")
      (elisp-lint-indent-specs (vulpea-utils-with-file . 1)
                               (vulpea-utils-with-note . 1)
                               (vulpea-utils-with-note-sync . 1)
-                              (org-with-point-at . 1)
-                              (org-element-map . 2))
+                              (org-with-point-at . 1) (org-element-map . 2))
      (eval when (fboundp 'rainbow-mode) (rainbow-mode 1))
      (qml-integration-qt-install-bin-folder . "~/Qt/6.9.1/gcc_64/bin")
      (squish-suites-folder . "squish_tests")
@@ -38,25 +33,20 @@
                                          "~/git_files/soft-clients/desktop_app/ui_session_common_qt/janus/in_session/resources/")
      (py-master-file
       . "/home/dmoreira/git_files/squish_tool/pcoip_client_control/tests/test_pages.py")
-     (qml-integration-ignored-paths "*/.*" "./Documentation"
-                                    "./package" "./sdk" "./Tools"
-                                    "./trust_agent_ipc" "./Dockerfile"
+     (qml-integration-ignored-paths "*/.*" "./Documentation" "./package" "./sdk"
+                                    "./Tools" "./trust_agent_ipc" "./Dockerfile"
                                     "./cmake" "./licenses" "./build*"
                                     "./modules/_submodules"
                                     "./modules/safe-c-library"
-                                    "./modules/logger"
-                                    "./modules/legacy"
-                                    "./modules/broker*"
-                                    "./modules/test*"
+                                    "./modules/logger" "./modules/legacy"
+                                    "./modules/broker*" "./modules/test*"
                                     "./modules/edid_retriever"
                                     "./modules/customization_example"
                                     "./modules/vchan_plugins"
                                     "./modules/tera_crypto"
-                                    "./modules/ndk_build"
-                                    "./modules/utils"
+                                    "./modules/ndk_build" "./modules/utils"
                                     "./modules/customization"
-                                    "./modules/layer_4"
-                                    "./modules/android"
+                                    "./modules/layer_4" "./modules/android"
                                     "./modules/high_performance_client"
                                     "*/localization" "*/translations")
      (qml-integration-qt-quick-controls-style . "imports/Janus")
@@ -65,24 +55,19 @@
      (qml-integration-user-styles "imports/Janus")
      (cmake-integration-include-subproject-targets-during-completion)
      (cmake-integration-hide-utility-targets-during-completion . t)
-     (cmake-integration-include-subproject-targets-during-completion
-      . t)
+     (cmake-integration-include-subproject-targets-during-completion . t)
      (cmake-integration-hide-library-targets-during-completion)
      (cmake-integration-run-working-directory . root)
      (org-download-method . attach) (org-download-method quote attach)
-     (ff-search-directories "." "../inc" "../src" "../api"
-                            "../src/view/impls/*" "../../inc"
-                            "../src/controller" "../../../../inc"
+     (ff-search-directories "." "../inc" "../src" "../api" "../src/view/impls/*"
+                            "../../inc" "../src/controller" "../../../../inc"
                             "/usr/include")
-     (ff-search-directories "." "../inc" "../src" "../api"
-                            "../src/view/impls/*" "../../inc"
+     (ff-search-directories "." "../inc" "../src" "../api" "../src/view/impls/*"
+                            "../../inc" "../../../../inc" "/usr/include")
+     (ff-search-directories "." "../inc" "../src" "../api" "../src/view/impls/*"
                             "../../../../inc" "/usr/include")
-     (ff-search-directories "." "../inc" "../src" "../api"
-                            "../src/view/impls/*" "../../../../inc"
-                            "/usr/include")
-     (ff-search-directories "." "../inc" "../src"
-                            "../src/view/impls/*" "../../../../inc"
-                            "/usr/include")
+     (ff-search-directories "." "../inc" "../src" "../src/view/impls/*"
+                            "../../../../inc" "/usr/include")
      (ff-search-directories "." "../inc" "../src" "../../../../inc"
                             "../src/view/impls/*" "/usr/include")
      (ff-search-directories "." "../inc" "../src" "../../../../inc"
@@ -103,8 +88,7 @@
      (my/qmlscene-extra-args . "-I ../janus/imports -I ../janus")
      (cmake-integration-build-dir . "build/Debug")
      (cmake-integration-hide-library-targets-during-completion . t)
-     (cmake-integration-conan-arguments
-      . "--build missing -r conancenter")
+     (cmake-integration-conan-arguments . "--build missing -r conancenter")
      (cmake-integration-hide-utility-targets-during-completion)
      (cmake-integration-include-conan-toolchain-file)
      (cmake-integration-include-conan-toolchain-file . t)
@@ -116,4 +100,4 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(my-org-double-caret-highlight ((t (:box (:line-width (2 . 2) :color "gray25" :style released-button))))))
