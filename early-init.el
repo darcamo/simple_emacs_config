a006603954fd5d57de25ba21cd6b74ad7cbedd89
Darlan Cavalcante Moreira <darcamo@gmail.com>
2026-09-03 20:42:56 -0300

    Increase TLS security

--- early-init.el
+++ early-init.el
@@ -1,2 +1,7 @@
 ;; -*- lexical-binding: t; -*-
 (setq package-enable-at-startup nil)
+
+;; See https://www.jamescherti.com/emacs-security-settings/
+(setq gnutls-verify-error t)
+(setq tls-checktrust t)
+(setq gnutls-min-prime-bits 3072)
